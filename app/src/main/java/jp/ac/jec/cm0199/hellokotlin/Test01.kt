fa94c3371e1448e7912756096ebb0ce1a841475c
LeoAndo <leo.777.info@gmail.com>
2025-09-25 09:46:21 +0900

    Add example variable declarations and comments in Test01.kt

--- app/src/main/java/jp/ac/jec/cm0199/hellokotlin/Test01.kt
+++ app/src/main/java/jp/ac/jec/cm0199/hellokotlin/Test01.kt
@@ -2,4 +2,8 @@ package jp.ac.jec.cm0199.hellokotlin
 
 fun main() {
     println("Hello, Kotlin")
+    // わざと変なコードを書いてコードレビューで指摘を受ける
+    var a = "Hello, Kotlin"
+    println(a)
+    val b = "Hello, Kotlin"
 }
