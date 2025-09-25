bbfff0046b8919c37e3bd55f05a9683b898bb2e2
LeoAndo <leo.777.info@gmail.com>
2025-09-25 16:09:26 +0900

    Add Main01.kt with example variable declarations and a main function

---
+++ app/src/main/java/jp/ac/jec/cm0199/hellokotlin/Main01.kt
@@ -0,0 +1,14 @@
+package jp.ac.jec.cm0199.hellokotlin
+
+fun main() {
+    val number1 = 1
+    val number2 = 2.0 // Double
+    val number3 = 3.0f // Float
+    var name = "Yamada"
+    name = "Tanaka"
+
+    println(number1)
+    println(number2)
+    println(number3)
+    println(name)
+}
